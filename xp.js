c6f1952728443052867f4d59223a8edd6175b491
Sky <178304360+blurskydev@users.noreply.github.com>
2024-08-18 14:31:58 +0700

    Update xp.js

--- xp.js
+++ xp.js
@@ -1,28 +1,25 @@
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
-const { getUserLanguages, headers, removeQuotes } = require('./helper.js')
+const { getUserLanguages, headers, removeQuotes } = require('./helper.js');
 
 const init = async () => {
-    const lessonsToComplete = process.env.lessonsToComplete ?? 5;
-    var token = process.env.token;
-    var userId = process.env.userId;
+    const lessonsToComplete = parseInt(process.env.lessonsToComplete) || 5;
+    let token = process.env.token;
+    let userId = process.env.userId;
 
     if (!token || !userId) {
-        throw new Error('You must specify a user ID and token.')
+        throw new Error('You must specify a user ID and token.');
     }
 
-    token = removeQuotes(token)
-    userId = removeQuotes(userId)
+    token = removeQuotes(token);
+    userId = removeQuotes(userId);
 
-    const userLanguages = await getUserLanguages()
-
-    console.log('Fetched User Languages: ', userLanguages)
+    const userLanguages = await getUserLanguages();
+    console.log('Fetched User Languages:', userLanguages);
 
     const attempt = async (formattedFraction) => {
         try {
-            const initialSessionBody = {
-                challengeTypes: [
-                    "listen",
-                ],
+            const sessionBody = {
+                challengeTypes: ["listen"],
                 fromLanguage: userLanguages.fromLanguage,
                 isFinalLevel: false,
                 isV2: true,
@@ -30,36 +27,34 @@ const init = async () => {
                 learningLanguage: userLanguages.learningLanguage,
                 levelIndex: 1,
                 shakeToReportEnabled: true,
-                skillId: "20017c47905904a4bbdfa3ca1b4bd85e", // Random Duolingo Skill ID
+                skillId: "20017c47905904a4bbdfa3ca1b4bd85e",
                 smartTipsVersion: 2,
-                type: "LEGENDARY_LEVEL"
-            }
+                type: "LEGENDARY_LEVEL",
+            };
 
             const createdSession = await fetch("https://www.duolingo.com/2017-06-30/sessions", {
                 headers,
-                body: JSON.stringify(initialSessionBody),
-                method: 'POST'
-            }).then((res) => {
+                method: 'POST',
+                body: JSON.stringify(sessionBody),
+            }).then(res => {
                 if (!res.ok) {
-                    throw new Error('Couldn\'t create fake session. Verify your auth credentials.')
+                    throw new Error('Could not create fake session. Verify your auth credentials.');
                 }
-                return res.json()
+                return res.json();
             });
 
-            console.log('Created Fake Duolingo Practice Session: ' + createdSession.id)
-
+            console.log(`Created Fake Duolingo Practice Session: ${createdSession.id}`);
 
             const rewards = await fetch(`https://www.duolingo.com/2017-06-30/sessions/${createdSession.id}`, {
                 headers,
+                method: 'PUT',
                 body: JSON.stringify({
                     ...createdSession,
                     beginner: false,
                     challengeTimeTakenCutoff: 6000,
-                    dailyRefreshInfo: null,
-                    startTime: (+new Date() - 60000) / 1000,
+                    startTime: (Date.now() - 60000) / 1000,
                     enableBonusPoints: true,
-                    explenations: {},
-                    endTime: +new Date() / 1000,
+                    endTime: Date.now() / 1000,
                     failed: false,
                     heartsLeft: 0,
                     hasBoost: true,
@@ -70,31 +65,29 @@ const init = async () => {
                     sessionExperimentRecord: [],
                     sessionStartExperiments: [],
                     showBestTranslationInGradingRibbon: true,
-                    xpPromised: 201 // Tell Duolingo that I was told I would get 200 xp
+                    xpPromised: 201,
                 }),
-                method: 'PUT'
-            }).then((res) => {
+            }).then(res => {
                 if (!res.ok) {
-                    return res.text().then((text) => {
-                        console.log(`There was an error recieving your rewards. RES: ${text}`)
-                    })
+                    return res.text().then(text => {
+                        console.log(`There was an error receiving your rewards. RES: ${text}`);
+                    });
                 }
-                return res.json()
-            })
+                return res.json();
+            });
 
-            console.log('Submitted Spoof Practice Session Data - Recieved')
-            console.log(`💪🏆🎉 Earned ${rewards.xpGain} XP!`)
+            console.log(`Submitted Spoof Practice Session Data - Received`);
+            console.log(`💪🏆🎉 Earned ${rewards.xpGain} XP!`);
         } catch (err) {
-            console.log('Error while getting XP for lesson: ' + formattedFraction + 'ERR: ' + err)
+            console.log(`Error while getting XP for lesson: ${formattedFraction} - ERR: ${err}`);
         }
+    };
+
+    for (let i = 0; i < lessonsToComplete; i++) {
+        const formattedFraction = `${i + 1}/${lessonsToComplete}`;
+        console.log(`Running: ${formattedFraction}`);
+        await attempt(formattedFraction);
     }
+};
 
-    for (var i = 0; i < lessonsToComplete; i++) {
-        const formattedFraction = i + 1 + '/' + lessonsToComplete;
-        console.log('Running: ' + formattedFraction)
-        await attempt(formattedFraction)
-    }
-
-}
-
-init()
+init();
